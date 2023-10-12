c8e7df1028bc657cc63399a90a9a7931245e5548
AkhilR9010 <127822128+AkhilR9010@users.noreply.github.com>
2023-10-12 23:22:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello Srinkanth Sir this is feature branch edit”);
+System.out.println(“hello Srinkanth Sir this is feature branch edit ..another edit done in remote repo github”);
 }
 }
 
